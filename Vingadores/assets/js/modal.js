467ac856d7c309d026a474e18575e1d1ffaee503
JVdomingos <87830728+JVdomingos@users.noreply.github.com>
2021-10-28 13:25:32 -0300

    Add files via upload

---
+++ Vingadores/assets/js/modal.js
@@ -0,0 +1,73 @@
+var isaiahBotao = document.querySelector("#isaiah__botao");
+var william = document.querySelector("#william__botao");
+var jeff = document.querySelector("#jeff__botao");
+var burnside = document.querySelector("#burnside__botao");
+var john = document.querySelector("#john__botao");
+var roscoe = document.querySelector("#roscoe__botao");
+var sharon = document.querySelector("#sharon__botao");
+var bucky = document.querySelector("#bucky__botao");
+
+isaiahBotao.addEventListener("click", function() {
+
+    var tituloModal = document.querySelector("#exampleModalLabel");
+    var modalConteudo = document.querySelector("#modalConteudo");
+
+    tituloModal.textContent = "Isaiah Bradley";
+    modalConteudo.textContent = "Ao contrário do que muitos acreditavam, em histórias mais atuais, foi revelado que o primeiro Capitão América na verdade foi Isaiah Bradley. Enfim, o soldado negro é o avô do Patriota, que é líder dos Jovens Vingadores.";
+})
+
+william.addEventListener("click", function() {
+    var tituloModal = document.querySelector("#exampleModalLabel");
+    var modalConteudo = document.querySelector("#modalConteudo");
+
+    tituloModal.textContent = "William Naslund";
+    modalConteudo.textContent = "Logo após o desaparecimento de Steve Rogers, o exército convocou William Naslund, o herói Espírito de 76, para assumir o manto. Em suma, ele continuou atuando depois da guerra e morreu ao salvar John Fitzgerald Kennedy de uma tentativa de assassinato, antes dele se tornar o presidente.";
+})
+
+jeff.addEventListener("click", function() {
+    var tituloModal = document.querySelector("#exampleModalLabel");
+    var modalConteudo = document.querySelector("#modalConteudo");
+
+    tituloModal.textContent = "Jeff Mace";
+    modalConteudo.textContent = "Ademais, Jeff Mace foi convocado para o lugar de William Naslund como novo Capitão e atuou até se aposentar.";
+})
+
+burnside.addEventListener("click", function() {
+    var tituloModal = document.querySelector("#exampleModalLabel");
+    var modalConteudo = document.querySelector("#modalConteudo");
+
+    tituloModal.textContent = "William Burnside";
+    modalConteudo.textContent = "Na virada dos anos 50 para 60, William Burnside descobriu a fórmula do soro do supersoldado e injetou em si mesmo. Entretanto, ele acabou ficando insano e se transformou no vilão Grande Diretor, que cometeu suicídio.";
+})
+
+john.addEventListener("click", function() {
+    var tituloModal = document.querySelector("#exampleModalLabel");
+    var modalConteudo = document.querySelector("#modalConteudo");
+
+    tituloModal.textContent = "John Walker";
+    modalConteudo.textContent = "Ademais, durante uma fase, Steve Rogers foi impedido pelo governo dos EUA de atuar como Capitão América. Então, nessa época, ele passou a atuar como o Capitão, com um uniforme preto. Assim, o cargo de Capitão América ficou com John Walker. No entanto, como John era muito violento, ele e Rogers acabaram entrando em confronto.";
+})
+
+roscoe.addEventListener("click", function() {
+    var tituloModal = document.querySelector("#exampleModalLabel");
+    var modalConteudo = document.querySelector("#modalConteudo");
+
+    tituloModal.textContent = "Roscoe Simons";
+    modalConteudo.textContent = "Por fim, na década de 1970, desiludido com o governo, o Capitão América abandona seu manto. Então, se torna no Nômade. Dessa forma, seu amigo Falcão treina um jovem chamado Roscoe para assumir o manto. Porém, o rapaz acaba sendo assassinado, vestido de Capitão América, pelas mãos do Caveira Vermelha.";
+})
+
+sharon.addEventListener("click", function() {
+    var tituloModal = document.querySelector("#exampleModalLabel");
+    var modalConteudo = document.querySelector("#modalConteudo");
+
+    tituloModal.textContent = "Sharon Rogers";
+    modalConteudo.textContent = "Diferentemente, de todos os outros Capitão América, Sharon Rogers foi a única mulher a receber esse título. Em suma, ela seria da Terra 415, onde Steve Rogers, seu pai, não teria sido congelado.";
+})
+
+bucky.addEventListener("click", function() {
+    var tituloModal = document.querySelector("#exampleModalLabel");
+    var modalConteudo = document.querySelector("#modalConteudo");
+
+    tituloModal.textContent = "Bucky";
+    modalConteudo.textContent = "Em outubro de 2007, o editor Joe Quesada da Marvel, anunciou que o Capitão América iria retornar.  No entanto, Bucky assumiu o manto, que revivera pouco antes como o super-herói Soldado Invernal e fora parceiro do Capitão na II Guerra Mundial. Pela primeira vez, o novo Capitão América de Bucky, iria usar como uma arma, além M1911A1 e uma faca, recursos cibernéticos para ampliar sua força no braço esquerdo.";
+})
